afa0ca504d4e4ee7bed54def77dd0365e55c48e7
Arnav Jain <arrnavjain@gmail.com>
2024-04-09 12:03:25 +0530

    fix(): minor enhancements

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,20 @@
 import React, { useState } from 'react'
 
-export default function About() {
-    const [style, setStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white',
-        border: '1px solid white'
-    });
+export default function About(props) {
+    // const [style, setStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white',
+    //     border: '1px solid white'
+    // });
 
-    const [buttonText, setButtonText] = useState("Enable Dark Mode");
-
-    const toggleStyle = () => {
-        if (style.color === 'white') {
-            setStyle({ color: 'black', backgroundColor: 'white' });
-            setButtonText("Enable Dark Mode")
-        } else {
-            setStyle({ color: 'white', backgroundColor: 'black', border: '1px solid white' })
-            setButtonText("Enable Light Mode")
-        }
+    let style = {
+        color: props.mode === 'dark' ? 'white' : "#042743",
+        backgroundColor: props.mode === 'dark' ? 'rgb(36, 74, 104)' : "white",
     }
+
     return (
-        <div className='container' style={style}>
-            <h1 className='my-3'>About Us</h1>
+        <div className='container'>
+            <h1 className='my-3' style={{color: props.mode === 'dark' ? 'white' : "#042743",}}>About Us</h1>
             <div className="accordion" id="accordionExample" style={style}>
                 <div className="accordion-item">
                     <h2 className="accordion-header">
@@ -58,9 +52,6 @@ export default function About() {
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="container my-3">
-                <button onClick={toggleStyle} type='button' className='btn btn-primary'>{buttonText}</button>
             </div>
         </div>
     )
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -43,16 +43,16 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode  === 'dark' ? 'white' : '#042743'}}></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleClearText}>Clear Text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
             <div className="container my-2" style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
                 <h1>Your text summary</h1>
-                <p>{text.split(" ").length} words, {text.length} characters</p>
-                <p>{text.split(" ").length * 0.008} Minutes read</p>
+                <p>{text.split(" ").filter((element) => element.length !== 0).length} words, {text.length} characters</p>
+                <p>{text.split(" ").filter((element) => element.length !== 0).length * 0.008} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : 'Enter something in the textbox to preview it here' }</p>
             </div>
